ccc992a3ac59ee84f2a5a0db2a6962a8535ee9d8
Lesiv Lyubomir <lyubas84@mail.ru>
2018-05-30 17:06:55 +0200

    go on

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,7 @@
 
   function myController($scope,$filter) {
     $scope.name = "Lesiv";
-    $scope.sayMessage = "Hello";
-    $scope.show = false;
-
-    $scope.showMessage = function(){
-      $scope.show = !$scope.show;
-    }
+    $scope.array = [1,2,3,4,5,6,7];
   }
 
 })();
